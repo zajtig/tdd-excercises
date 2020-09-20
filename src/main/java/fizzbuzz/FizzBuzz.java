43dba5380f8f903adb63a45497a92363ad161359
Zajti Gábor <Gabor.Zajti@idomsoft.hu>
2020-09-20 16:49:01 +0200

    3. Generate first 15 elements

--- src/main/java/fizzbuzz/FizzBuzz.java
+++ src/main/java/fizzbuzz/FizzBuzz.java
@@ -5,20 +5,25 @@ import java.util.List;
 
 public class FizzBuzz {
 
-    public List<String> getFizzBuzzSequence(int endInclusive) {
+    public static final int INCLUSIVE_THRESHOLD = 15;
+
+    public List<String> getFizzBuzzSequence() {
         List<String> result = new ArrayList<>();
-        for (int i = 1; i <= endInclusive; i++) {
+        for (int i = 1; i <= INCLUSIVE_THRESHOLD; i++) {
             result.add(generateNumberOrFizzOrBuzz(i));
         }
         return result;
     }
 
     private String generateNumberOrFizzOrBuzz(int number) {
-        if (number % 3 == 0) {
-            return "Fizz";
+        if (number % 15 == 0) {
+            return "FizzBuzz";
         }
         if (number % 5 == 0) {
             return "Buzz";
+        }
+        if (number % 3 == 0) {
+            return "Fizz";
         }
         return Integer.toString(number);
     }
--- src/test/java/fizzbuzz/FizzBuzzUnitTest.java
+++ src/test/java/fizzbuzz/FizzBuzzUnitTest.java
@@ -1,15 +1,17 @@
 package fizzbuzz;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.List;
 
-public class FizzBuzzUnitTest {
+import static org.junit.Assert.assertEquals;
 
+public class FizzBuzzUnitTest {
     @Test
-    public void generateFirst14ElementsTest() {
-        Assert.assertEquals(List.of("1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14")
-                , new FizzBuzz().getFizzBuzzSequence(14));
+    public void generateFirst15ElementsTest() {
+        List<String> expectedResult = List.of(
+                "1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz",
+                "11", "Fizz", "13", "14", "FizzBuzz");
+        assertEquals(expectedResult, new FizzBuzz().getFizzBuzzSequence());
     }
 }
