5aacfc843bc5736ea6f4bb756e24aaeabac51c0f
Zajti Gábor <Gabor.Zajti@idomsoft.hu>
2020-09-20 16:40:17 +0200

    2. Generate first 14 elements

--- src/main/java/fizzbuzz/FizzBuzz.java
+++ src/main/java/fizzbuzz/FizzBuzz.java
@@ -5,17 +5,20 @@ import java.util.List;
 
 public class FizzBuzz {
 
-    public List<String> generateFirstTreeNumbers() {
+    public List<String> getFizzBuzzSequence(int endInclusive) {
         List<String> result = new ArrayList<>();
-        for (int i = 1; i < 4; i++) {
-            result.add(generateNumberOrFizz(i));
+        for (int i = 1; i <= endInclusive; i++) {
+            result.add(generateNumberOrFizzOrBuzz(i));
         }
         return result;
     }
 
-    private String generateNumberOrFizz(int number) {
+    private String generateNumberOrFizzOrBuzz(int number) {
         if (number % 3 == 0) {
             return "Fizz";
+        }
+        if (number % 5 == 0) {
+            return "Buzz";
         }
         return Integer.toString(number);
     }
--- src/test/java/fizzbuzz/FizzBuzzUnitTest.java
+++ src/test/java/fizzbuzz/FizzBuzzUnitTest.java
@@ -8,7 +8,8 @@ import java.util.List;
 public class FizzBuzzUnitTest {
 
     @Test
-    public void generateFirstTreeNumberTest() {
-        Assert.assertEquals(List.of("1", "2", "Fizz"), new FizzBuzz().generateFirstTreeNumbers());
+    public void generateFirst14ElementsTest() {
+        Assert.assertEquals(List.of("1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14")
+                , new FizzBuzz().getFizzBuzzSequence(14));
     }
 }
