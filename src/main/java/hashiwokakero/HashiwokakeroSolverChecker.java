5f9f9e99fa736a10be38cad5958971cbb78f89be
Zajti Gábor <Gabor.Zajti@idomsoft.hu>
2020-09-20 17:39:38 +0200

    0. HashiwokakeroSolverChecker create test

---
+++ src/main/java/hashiwokakero/HashiwokakeroSolverChecker.java
@@ -0,0 +1,4 @@
+package hashiwokakero;
+
+public class HashiwokakeroSolverChecker {
+}
---
+++ src/test/java/hashiwokakero/HashiwokakeroSolverCheckerTest.java
@@ -0,0 +1,12 @@
+package hashiwokakero;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HashiwokakeroSolverCheckerTest {
+
+    @Test
+    public void createTest() {
+        Assert.assertNotNull(new HashiwokakeroSolverChecker());
+    }
+}
