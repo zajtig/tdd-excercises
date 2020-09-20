5fd439634d5d6e017206ab61c605a5984eaf0e47
Zajti Gábor <Gabor.Zajti@idomsoft.hu>
2020-09-20 17:08:48 +0200

    4. Generate first 100 elements

--- src/main/java/fizzbuzz/FizzBuzz.java
+++ src/main/java/fizzbuzz/FizzBuzz.java
@@ -1,30 +1,41 @@
 package fizzbuzz;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 public class FizzBuzz {
 
-    public static final int INCLUSIVE_THRESHOLD = 15;
+    private static final int FIRST_ELEMENT = 1;
+    private static final int LATEST_ELEMENT = 100;
+
+    private static final Map<Integer, String> SPECIFIC_ELEMENTS = new LinkedHashMap<>();
+
+    static {
+        SPECIFIC_ELEMENTS.put(15, "FizzBuzz");
+        SPECIFIC_ELEMENTS.put(5, "Buzz");
+        SPECIFIC_ELEMENTS.put(3, "Fizz");
+    }
 
     public List<String> getFizzBuzzSequence() {
         List<String> result = new ArrayList<>();
-        for (int i = 1; i <= INCLUSIVE_THRESHOLD; i++) {
+        for (int i = FIRST_ELEMENT; i <= LATEST_ELEMENT; i++) {
             result.add(generateNumberOrFizzOrBuzz(i));
         }
         return result;
     }
 
     private String generateNumberOrFizzOrBuzz(int number) {
-        if (number % 15 == 0) {
-            return "FizzBuzz";
-        }
-        if (number % 5 == 0) {
-            return "Buzz";
-        }
-        if (number % 3 == 0) {
-            return "Fizz";
+        for (Map.Entry<Integer, String> entry : SPECIFIC_ELEMENTS.entrySet()) {
+            if (isFizzBuzzSpecificElement(number, entry.getKey())) {
+                return entry.getValue();
+            }
         }
         return Integer.toString(number);
+    }
+
+    private boolean isFizzBuzzSpecificElement(int number, int specificElement) {
+        return number % specificElement == 0;
     }
 }
