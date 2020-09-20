c9687ca90f5cb61955e6e2a60937fc32ebe46c4a
Zajti Gábor <Gabor.Zajti@idomsoft.hu>
2020-09-20 15:41:05 +0200

    0. fizzbuzz create check

---
+++ src/test/java/fizzbuzz/FizzBuzzUnitTest.java
@@ -0,0 +1,13 @@
+package fizzbuzz;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class FizzBuzzUnitTest {
+
+    @Test
+    public void createTest() {
+        Assert.assertNotNull(new FizzBuzz());
+    }
+}
